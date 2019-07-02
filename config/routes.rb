c06294f4f068c70794d7b659d4ef4a25bf27071b
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-02 01:35:11 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-rsRails.application.routes.draw do
+Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :students, only: [:index, :show, :new, :create, :edit, :update]
 
