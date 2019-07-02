91c2c81193ade37ce216755e4ad4abe712fbb995
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-02 00:31:02 +0000

    Automatically backed up by Learn

--- app/controllers/student_classes.rb
+++ app/controllers/student_classes.rb
@@ -1,5 +1,38 @@
 class StudentClassesController < ApplicationController
 
+ def index
+		@students = Student.all
+	end
 
+	def show
+		@student = Student.find(params[:id])
+	end
+
+	def new
+		@student = Student.new
+	end
+
+	def create
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+	end
+
+	def update
+    @student = Student.find(params[:id])
+    @student.update(student_params(:title))
+    redirect_to student_path(@student)
+	end
+
+	def edit
+	  @student = Student.find(params[:id])
+	end
+end
+
+  private
+ 
+  def student_params(*args)
+    params.require(:student).permit(*args)
+  end
 
 end
